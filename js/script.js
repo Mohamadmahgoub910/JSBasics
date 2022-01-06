d1385e5d89c3782291a8df4c4f64157b36c6e6e5
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2022-01-06 10:54:10 +0200

    array & object & DOM

--- js/script.js
+++ js/script.js
@@ -1,3 +1,82 @@
+// h1 = document.getElementsByTagName("h1")[0];
+// console.log(h1);
+h11 = document.getElementById("first_h1");
+// fire = document.getElementById("fire");
+function fired() {
+  h11.style.color = "white";
+  h11.style.backgroundColor = "gray";
+  console.log(h11);
+  console.log("applied");
+}
+function over() {
+  h11.style.color = "white";
+  h11.style.backgroundColor = "green";
+  console.log("mouse is over");
+}
+function out() {
+  h11.style.color = "white";
+  h11.style.backgroundColor = "gray";
+  console.log("out applied");
+}
+div1 = document.getElementById("div1");
+div1.onclick = () => {
+  console.log("div is fired");
+};
+div1.title = "hello";
+console.log(div1.title);
+// Read array of object data
+// let students = [
+//   {
+//     id: 1,
+//     name: "ali",
+//     age: 20,
+//   },
+//   {
+//     id: 2,
+//     name: "aya",
+//     age: 24,
+//   },
+//   {
+//     id: 3,
+//     name: "aliaa",
+//     age: 30,
+//   },
+// ];
+//  All Students
+// console.log(students);
+// loop array first
+// students.forEach((student) => {
+//   console.log(student);
+//   let x;
+//   document.write("<table>");
+//   document.write("<tr>");
+//   for (x in student) {
+//     console.log(`${x}, ${student[x]}`);
+
+//     document.write("<td>" + `${x}, ${student[x]}` + "</td>");
+//   }
+//   document.write("<tr>");
+//   document.write("</table>");
+// });
+
+// / //////////////////////
+// convert array into object and object into an array
+// let makeObj = {
+//   gra: ["one", "two", "three"],
+//   stuff: ["s1", "s2", "s3"],
+//   Qty: [1, 2, 3],
+// };
+// // print all elements in the array from an object
+// console.log(makeObj.stuff);
+// makeObj.stuff.forEach(function (ele) {
+//   console.log(ele);
+// });
+// // print all elements in the object { indexes + value[indexes]}
+// let x;
+// for (x in makeObj) {
+//   console.table(`index ${x} and values are ${makeObj[x]}`);
+// }
+// ////////////////////////////////
 // For with object
 // var aObj = {
 //   a: 1,
