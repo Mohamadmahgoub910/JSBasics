13b05c6edb35133f3cdafb4c0c62c324c230685c
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2022-01-11 05:02:47 +0200

    handle next and previous

--- Ma7goubSpinxTask/js/script.js
+++ Ma7goubSpinxTask/js/script.js
@@ -1,3 +1,38 @@
 $(document).ready(function () {
   $("h3").css("color", "green");
+  // next
+  // Event
+  $(".next").one("click", function () {
+    $(".Q1").css({ display: "none" });
+    $(".Q2").css({ display: "block" });
+    $(".next").addClass("disabled");
+  });
+  // ///////////////////////////////////////////
+
+  // prev
+  // Event
+  $(".previous").one("click", function () {
+    $(".Q2").css({ display: "none" });
+    $(".Q1").css({ display: "block" });
+    $(".previous").addClass("disabled");
+  });
+  // ///////////////////////////////////////////
+
+  /**
+    * document.getElementById("page1").style.display="none";
+
+    document.getElementById("page2").style.display="block";
+    $('#page2').addClass('active');
+    $('#page1').removeClass('active');
+
+
+    $('.next').addClass('disabled');
+
+    $('.back').removeClass('disabled');
+
+    document.getElementById("page-no").textContent="2 of 2";
+    */
+  // prev
+  // Q1
+  // Q2
 });
