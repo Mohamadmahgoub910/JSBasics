17182fb0ce0175a620a5a78617ed9ea9e7fe5663
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2022-01-11 04:14:45 +0200

    all design successfully done!

--- Ma7goubSpinxTask/js/script.js
+++ Ma7goubSpinxTask/js/script.js
@@ -1,9 +1,3 @@
 $(document).ready(function () {
   $("h3").css("color", "green");
-  // hide question use hideData
-  // Event Then Action
-  // $(".panel-body").one("click", function () {
-  //   $(".insertAfter").after("wrong choice");
-  // });
-  // Pagination
 });
