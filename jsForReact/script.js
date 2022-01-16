2db8d2cf3d2cf0d45bf06e6cc563b3610f3fd49a
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2022-01-16 10:45:04 +0200

    template literal

---
+++ jsForReact/script.js
@@ -0,0 +1,7 @@
+// Template literal / Strings
+console.log("Hi Baby !");
+var aliName = "ali";
+var age = 20;
+console.log(`my name is  ${aliName} 
+and my age is ${age} 
+so ${age > 30 ? "med" : "less than 30"}`);
